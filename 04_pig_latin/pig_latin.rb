3eff378646a06aa1c5ce58fd5b230940d3212cf2
nick2383 <nicholas.birch2@gmail.com>
2016-03-12 15:05:55 -0800

    replaced ifs with a while

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,23 +1,36 @@
 # 03_simon_says
 # Author: Nick Birch
 
+# def translate(sentence)
+# 	vowels = ["a", "e", "i", "o", "u"]
+# 	sentence = sentence.split(" ")
+# 	sentence = sentence.collect do |word|
+# 		if (vowels.include?(word.byteslice(0).downcase))
+# 			word + "ay"
+# 		else
+# 			if !(vowels.include?(word.byteslice(1).downcase))
+# 				word = (word + word.byteslice(0,2).downcase + "ay")
+# 				word.slice!(0,2)
+# 				word
+# 			else
+# 				word = (word + word.byteslice(0).downcase + "ay")
+# 				word.slice!(0)
+# 				word
+# 			end	
+# 		end
+# 	end
+# 	sentence.join(" ")
+# end
+
 def translate(sentence)
 	vowels = ["a", "e", "i", "o", "u"]
 	sentence = sentence.split(" ")
 	sentence = sentence.collect do |word|
-		if (vowels.include?(word.byteslice(0).downcase))
-			word + "ay"
-		else
-			if !(vowels.include?(word.byteslice(1).downcase))
-				word = (word + word.byteslice(0,2).downcase + "ay")
-				word.slice!(0,2)
-				word
-			else
-				word = (word + word.byteslice(0).downcase + "ay")
-				word.slice!(0)
-				word
-			end	
+		while(!(vowels.include?(word.byteslice(0))))
+			word = word + word.byteslice(0)
+			word.slice!(0)
 		end
+		word = word + "ay"
 	end
 	sentence.join(" ")
 end
